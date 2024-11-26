8317c2e16c602a7fd7a37181465709adabf2446e
Jack Knox <knox36@purdue.edu>
2024-11-25 20:18:08 -0500

    Update endpoints

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.use("/", (req, res) => {
-    res.send("Server is running :)")
-})
+// app.use("/", (req, res) => {
+//     res.send("Server is running...")
+// })
+
+app.get("/api/data", (req, res) => {
+    res.json({
+        message: "Hello from the API!",
+        success: true,
+        data: [1, 2, 3, 4, 5]
+    });
+});
 
 app.listen(5000, console.log("Server started..."))
